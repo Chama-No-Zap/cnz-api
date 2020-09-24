6dec8d70a8cc8a74f88b0d0e31494ae94e68f6b5
Cardoso <lsc.cardosoo@gmail.com>
2020-09-23 21:44:36 -0300

    Adicionar aos schemas de users regras para inserçao de novo usario no banco

--- src/users/schemas.js
+++ src/users/schemas.js
@@ -3,19 +3,41 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: { type: String, require: true},
-  // // cpf validado
-  // cpf: String,
-  // phone: String,
-  // address: object({
-  //   // validação do cep
-  //   cep: String,
-  //   number: number().required(),
-  //   complement: String,
-  // }).required(),
-  // purchaseHistory: [
-  //   // ids
-  // ]
+  name: {
+    type: String,
+    require: true
+  },
+  cpf: {
+    type: String,
+    minlength: 11,
+    require: true,
+    maxlength: 14,
+  },
+  phone: {
+    type: String,
+    require: true
+  },
+  address: {
+    type: Object,
+    require: true
+  },
+  complement: {
+    type: String,
+    require: true
+  },
 });
+
+// // cpf validado
+// cpf: String,
+// phone: String,
+// address: object({
+//   // validação do cep
+//   cep: String,
+//   number: number().required(),
+//   complement: String,
+// }).required(),
+// purchaseHistory: [
+//   // ids
+// ]
 
 module.exports = mongoose.model("Users", userSchema);
