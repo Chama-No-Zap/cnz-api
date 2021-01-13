f6f37573c3d42a4cc03a25a32edf8200b989d8b0
Cardoso <lsc.cardosoo@gmail.com>
2021-01-13 15:47:22 -0300

    Finalizar refatoraçao de crud usuario

--- src/users/usersController.js
+++ src/users/usersController.js
@@ -1,38 +1,34 @@
 const { Router } = require('express');
-const rescue = require('express-rescue');
+// const rescue = require('express-rescue');
 const usersService = require('./usersService');
-const removeSpecialsCharacters = require('../middlewares/removeSpecialsCharacters');
+// const removeSpecialsCharacters = require('../middlewares/removeSpecialsCharacters');
 const verifyParams = require('../middlewares/verifyParams');
 
 const usersRouter = Router();
 
-// espera-se que `data` tenha duas propriedades:
-// title: indica o que está sendo criado
-// content: conteúdo que deve ser adicionado ao campo.
 const createUser = async (req, res, _next) => {
   const { data } = req.body;
   try {
     const response = await usersService.createUser(data);
     return res.status(201).json(response);
-  } catch (error) {
-    console.log(error);
-    res.status(400).json(error);
+  } catch ({ code = 400, ...error }) {
+    res.status(code).json(error);
   }
 };
 
-// const getUserByPhone = (async (req, res, _next) => {
-//   const { data: { phone } } = req.body;
-//   try {
-//     const response = await usersService.getUserByPhone(phone);
-//     return res.status(200).json(response);
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
+const getUserByPhone = (async (req, res, _next) => {
+  const { data } = req.body;
+  try {
+    const response = await usersService.getUserByPhone(data);
+    return res.status(200).json(response);
+  } catch ({ code = 400, ...error }) {
+    res.status(code).json(error);
+  }
+});
 
 usersRouter
   .route('/')
   .post(verifyParams, createUser)
-  // .get(getUserByPhone);
+  .get(verifyParams, getUserByPhone);
 
 module.exports = usersRouter;
