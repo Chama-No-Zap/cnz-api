06ff6d0b8c8e77b0b27389af3442d676f2a9a0cf
Cardoso <lsc.cardosoo@gmail.com>
2021-01-14 20:54:34 -0300

    Adicionar variaveis de ambiente

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const express = require('express');
 const mongoose = require('mongoose');
+const express = require('express');
 const bodyParser = require('body-parser');
 const usersRouter = require('./src/users/usersController');
-const sellersRouter = require('./src/sellers/sellersController');
 const errorHandler = require('./src/middlewares/errorHandler');
+require('dotenv/config');
+// const sellersRouter = require('./src/sellers/sellersController');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const DB_URL = process.env.DB_URL || 'mongodb://localhost/cnz';
 
-mongoose.connect('mongodb://localhost/cnz', {
+mongoose.connect(DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
@@ -19,7 +21,7 @@ app.use(bodyParser.json());
 
 app.use('/users', usersRouter);
 
-app.use('/sellers', sellersRouter);
+// app.use('/sellers', sellersRouter);
 app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Listenning on PORT ${PORT}`));
--- src/middlewares/verifyParams.js
+++ src/middlewares/verifyParams.js
@@ -2,6 +2,8 @@ const { BAD_REQUEST_FORMAT } = require('../errors');
 
 const verifyParams = (req, res, next) => {
   const { data = {} } = req.body;
+  console.log(req.body)
+  // return res.status(200).json({ recebido: req.body} );
   if (!data.title || !data.content) {
     return res.status(BAD_REQUEST_FORMAT.code).json(BAD_REQUEST_FORMAT);
   }
