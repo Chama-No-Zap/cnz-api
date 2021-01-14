a180b1ab1e5916a39a056793cc8941c51c9d6236
Cardoso <lsc.cardosoo@gmail.com>
2021-01-14 12:25:45 -0300

    Testes de integraçao

--- test/integration/users.test.js
+++ test/integration/users.test.js
@@ -1,9 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const { requestions, validUser, requestionsWithError } = require('../../mock/index');
+const { requestions, requestionsWithError } = require('../../mock/index');
 const User = require('../../src/users/usersModel');
-const server = require('../../index');
-const { BAD_REQUEST_FORMAT } = require('../../src/errors');
 
 chai.use(chaiHttp);
 chai.should();
@@ -63,6 +61,8 @@ describe('API para cadastro e atualização de usuário', () => {
       });
     });
   });
+});
+
   // describe('', () => {
   //   it('', () => {
   //         // it ('Testar que é possível adicionar CEP à conta do usuário', (done) => {
@@ -82,4 +82,3 @@ describe('API para cadastro e atualização de usuário', () => {
   //   })
 
   // })
-});
